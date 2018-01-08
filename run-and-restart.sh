dc9ae129d7f950914c5b15e5d9f2706fe2e32995
David McGillicuddy <contact@djmcgill.co.uk>
2018-01-08 21:16:18 +0000

    change quotes in docker restart

--- run-and-restart.sh
+++ run-and-restart.sh
@@ -1,4 +1,4 @@
-CMD='docker run -it --rm --volume "/home/ec2-user/dominions-5-status/resources":"/usr/src/myapp/resources" dom-5-bot'
+CMD='docker run -it --rm --volume \"/home/ec2-user/dominions-5-status/resources\":\"/usr/src/myapp/resources\" dom-5-bot'
 
 while true; do
     $CMD
