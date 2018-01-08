3e929414a59a5f307b6be6a1adb11736d85158d8
David McGillicuddy <contact@djmcgill.co.uk>
2018-01-08 21:13:51 +0000

    correct dockerfile

--- run-and-restart.sh
+++ run-and-restart.sh
@@ -1,6 +1,6 @@
-CMD='docker run -it -rm --volume "/home/ec2-user/dominions-5-status/resources":"/usr/src/myapp/resources" dom-5-bot'
+CMD='docker run -it --rm --volume "/home/ec2-user/dominions-5-status/resources":"/usr/src/myapp/resources" dom-5-bot'
 
 while true; do
     $CMD
-    SLEEP 60
+    sleep 60
 done
