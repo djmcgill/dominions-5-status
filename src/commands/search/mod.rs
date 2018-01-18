83dd87081a18054dba01c34bfb1313c7ac2a5a09
David McGillicuddy <contact@djmcgill.co.uk>
2018-01-18 14:33:11 +0000

    staticise the InspectorCategory enum

--- src/commands/search/mod.rs
+++ src/commands/search/mod.rs
@@ -3,25 +3,56 @@ use url::percent_encoding::{utf8_percent_encode, QUERY_ENCODE_SET};
 use serenity::framework::standard::{Args, CommandError};
 use serenity::model::Message;
 
-const VALID: &[&'static str] = &["item", "spell", "unit", "site", "merc", "event"]; // TODO: hashset?
+// TODO: implement some kind of static enum macro or library
+trait InspectorCategory: Copy {
+    fn show() -> &'static str;
+    fn event_append() -> &'static str;
+}
 
-pub fn search(category: &str, message: &Message, args: Args) -> Result<(), CommandError> {
+#[derive(Clone, Copy)]
+struct Item;
+impl InspectorCategory for Item {
+    fn show() -> &'static str { "item" }
+    fn event_append() -> &'static str { "" }
+}
+#[derive(Clone, Copy)]
+struct Spell;
+impl InspectorCategory for Spell {
+    fn show() -> &'static str { "spell" }
+    fn event_append() -> &'static str { "" }
+}
+#[derive(Clone, Copy)]
+struct Unit;
+impl InspectorCategory for Unit {
+    fn show() -> &'static str { "unit" }
+    fn event_append() -> &'static str { "" }
+}
+#[derive(Clone, Copy)]
+struct Site;
+impl InspectorCategory for Site {
+    fn show() -> &'static str { "site" }
+    fn event_append() -> &'static str { "" }
+}
+#[derive(Clone, Copy)]
+struct Merc;
+impl InspectorCategory for Merc {
+    fn show() -> &'static str { "merc" }
+    fn event_append() -> &'static str { "" }
+}
+#[derive(Clone, Copy)]
+struct Event;
+impl InspectorCategory for Event {
+    fn show() -> &'static str { "event" }
+    fn event_append() -> &'static str { "&loadEvents=1" }
+}
+
+fn search<I: InspectorCategory>(_category: I, message: &Message, args: Args) -> Result<(), CommandError> {
     let search_term = utf8_percent_encode(&args.full(), QUERY_ENCODE_SET).to_string();
-    if category == VALID[0] || category == VALID[1] ||
-        category == VALID[2] || category == VALID[3] ||
-        category == VALID[4] { // TODO: so gross
-        let response = format!(
-            "https://larzm42.github.io/dom5inspector/?page={}&{}q={}&showmodcmds=1&showmoddinginfo=1&showids=1",
-        category, category, search_term);
-        info!("responding with {}", response);
-        let _ = message.reply(&response); 
-    } else if category == VALID[5] { // TODO: so grosser
-                let response = format!(
-            "https://larzm42.github.io/dom5inspector/?page={}&{}q={}&showmodcmds=1&showmoddinginfo=1&showids=1&loadEvents=1",
-        category, category, search_term);
-        info!("responding with {}", response);
-        let _ = message.reply(&response); 
-    };
+    let response = format!(
+        "https://larzm42.github.io/dom5inspector/?page={}&{}q={}&showmodcmds=1&showmoddinginfo=1&showids=1{}",
+    I::show(), I::show(), search_term, I::event_append());
+    info!("responding with {}", response);
+    let _ = message.reply(&response); 
     Ok(())
 }
 
@@ -29,30 +60,31 @@ use serenity::framework::standard::StandardFramework;
 pub trait WithSearchCommands: Sized {
     fn get_standard_framework(self) -> StandardFramework;
     fn with_search_commands(self) -> StandardFramework {
-        self.get_standard_framework().command("item", |c| c
-            .bucket("simple")
-            .exec(|_, m, a| search(&"item", m, a))
-        )
-        .command("spell", |c| c
-            .bucket("simple")
-            .exec(|_, m, a| search(&"spell", m, a))
-        )
-        .command("unit", |c| c
-            .bucket("simple")
-            .exec(|_, m, a| search(&"unit", m, a))
-        )
-        .command("site", |c| c
-            .bucket("simple")
-            .exec(|_, m, a| search(&"site", m, a))
-        )
-        .command("merc", |c| c
-            .bucket("simple")
-            .exec(|_, m, a| search(&"merc", m, a))
-        )
-        .command("event", |c| c
-            .bucket("simple")
-            .exec(|_, m, a| search(&"event", m, a))
-        )
+        self.get_standard_framework()
+            .command(Item::show(), |c| c
+                .bucket("simple")
+                .exec(|_, m, a| search(Item, m, a))
+            )
+            .command(Spell::show(), |c| c
+                .bucket("simple")
+                .exec(|_, m, a| search(Spell, m, a))
+            )
+            .command(Unit::show(), |c| c
+                .bucket("simple")
+                .exec(|_, m, a| search(Unit, m, a))
+            )
+            .command(Site::show(), |c| c
+                .bucket("simple")
+                .exec(|_, m, a| search(Site, m, a))
+            )
+            .command(Merc::show(), |c| c
+                .bucket("simple")
+                .exec(|_, m, a| search(Merc, m, a))
+            )
+            .command(Event::show(), |c| c
+                .bucket("simple")
+                .exec(|_, m, a| search(Event, m, a))
+            )
     }
 }
 impl WithSearchCommands for StandardFramework {
