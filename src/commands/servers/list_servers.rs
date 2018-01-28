63b88e15ac327eeeab229639606473ca68b1d406
David McGillicuddy <contact@djmcgill.co.uk>
2018-01-28 20:37:14 +0000

    add embed to list

--- src/commands/servers/list_servers.rs
+++ src/commands/servers/list_servers.rs
@@ -9,18 +9,33 @@ pub fn list_servers(context: &mut Context, message: &Message) -> Result<(), Comm
     let data = context.data.lock();
     let db_conn = data.get::<DbConnectionKey>().ok_or_else(|| CommandError("No db connection".to_string()))?;
     let server_list = db_conn.retrieve_all_servers().map_err(CommandError::from)?;
-    let mut text = String::new();
-    text.push_str(&"Servers:\n");
+    let embed_title = "Servers:";
+    let mut server_aliases = String::new();
+    let mut server_addresses = String::new();
     for server in server_list {
         match server.state {
-            GameServerState::Lobby(_) => 
-                text.push_str(&format!("{} (-)\n", server.alias)),
-            
-            GameServerState::StartedState(started_state, _) => 
-                text.push_str(&format!("{} ({})\n", server.alias, started_state.address)),
+            GameServerState::Lobby(_) => {
+                server_aliases.push_str(&format!("{}\n", server.alias));
+                server_addresses.push_str(&"-\n");
+            }
+            GameServerState::StartedState(started_state, _) => {
+                server_aliases.push_str(&format!("{}\n", server.alias));
+                server_addresses.push_str(&format!("{}\n", started_state.address));
+            }
         }
-
     }
-    message.reply(&text)?;
+    message.channel_id.send_message(|m| m
+        .embed(|e| e
+            .title(embed_title)
+            .field( |f| f
+                .name("Alias")
+                .value(server_aliases)
+            )
+            .field ( |f| f
+                .name("Address")
+                .value(server_addresses)
+            )
+        )
+    )?;
     Ok(())
 }
