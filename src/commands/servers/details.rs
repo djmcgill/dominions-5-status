ec4b1e167de509fa0e58c858fd1f6e2bbfb2d763
David McGillicuddy <contact@djmcgill.co.uk>
2018-01-28 19:58:09 +0000

    give lobby details an embed response

--- src/commands/servers/details.rs
+++ src/commands/servers/details.rs
@@ -3,10 +3,11 @@ use ::server::get_game_data;
 use serenity::framework::standard::{Args, CommandError};
 use serenity::prelude::Context;
 use serenity::model::Message;
+use serenity::builder::CreateEmbed;
 
-use model::GameServerState;
+use model::{GameServerState, LobbyState, StartedState};
 use model::enums::{Nations, NationStatus};
-use db::DbConnectionKey;
+use db::{DbConnection, DbConnectionKey};
 
 pub fn details(context: &mut Context, message: &Message, mut args: Args) -> Result<(), CommandError> {
     let data = context.data.lock();
@@ -19,85 +20,129 @@ pub fn details(context: &mut Context, message: &Message, mut args: Args) -> Resu
     }
     let server = db_conn.game_for_alias(&alias)?;
 
-    match server.state {
-        GameServerState::Lobby(_) => {
-            let mut text = format!("{} (Lobby)\nPlayers\n", server.alias);
-            for (player, nation_id) in db_conn.players_with_nations_for_game_alias(&alias)? {
-                let &(nation_name, era) = Nations::get_nation_desc(nation_id);
-                text.push_str(&format!("{}: {} {}\n", player.discord_user_id.get()?, era, nation_name));
-            }
-            message.reply(&text)?;
+    let embed_response = match server.state {
+        GameServerState::Lobby(lobby_state) => {
+            lobby_details(
+                db_conn,
+                lobby_state,
+                &alias,
+            )?
         }
         GameServerState::StartedState(started_state) => {
-            let ref server_address = started_state.address;
-            let mut game_data = get_game_data(&server_address)?;
-            game_data.nations.sort_unstable_by(|a, b| a.name.cmp(&b.name));
-
-            let mut nation_names = String::new();
-            let mut player_names = String::new();
-            let mut submitted_status = String::new();
-
-            let id_player_nations = db_conn.players_with_nations_for_game_alias(&alias)?;
-
-            for nation in &game_data.nations {
-                debug!("Creating format for nation {} {}", nation.era, nation.name);
-                nation_names.push_str(&format!("{} {}\n", nation.era, nation.name));
-
-                let nation_string = if let NationStatus::Human = nation.status {
-                    if let Some(&(ref player, _)) = id_player_nations.iter().find(
-                        |&&(_, nation_id)| nation_id == nation.id
-                        ) {
-                            format!("**{}**", player.discord_user_id.get()?)    
-                        } else {
-                            nation.status.show().to_string()
-                        }
-
-                } else {
-                    nation.status.show().to_string()
-                };
-
-                player_names.push_str(&format!("{}\n", nation_string));
-                
-                if let NationStatus::Human = nation.status {
-                    submitted_status.push_str(&format!("{}\n", nation.submitted.show()));
-                } else {
-                    submitted_status.push_str(&".\n");
-                }
-            }
-            info!("Server details string created, now sending.");
-            let total_mins_remaining = game_data.turn_timer / (1000*60);
-            let hours_remaining = total_mins_remaining/60;
-            let mins_remaining = total_mins_remaining - hours_remaining*60;
-
-            let embed_title = format!("{}: turn {}, {}h {}m remaining",
-                        game_data.game_name,
-                        game_data.turn,
-                        hours_remaining,
-                        mins_remaining);
-
-            info!("replying with embed_title {:?}\n nations {:?}\n players {:?}\n, submission {:?}",
-            embed_title, nation_names, player_names, submitted_status);
-
-            message.channel_id.send_message(|m| m
-                .embed(|e| e
-                    .title(embed_title)
-                    .field( |f| f
-                        .name("Nation")
-                        .value(nation_names)
-                    )
-                    .field ( |f| f
-                        .name("Player")
-                        .value(player_names)
-
-                    )
-                    .field ( |f| f
-                        .name("Submitted")
-                        .value(submitted_status)
-                    )
-                )        
-            )?;
+            started_details(
+                db_conn,
+                started_state,
+                &alias,
+            )?
         }
     };
-    Ok(())
-    
+    message.channel_id.send_message(|m| m
+        .embed(|_| embed_response)
+    )?;
+    Ok(())   
+}
+
+fn lobby_details(
+    db_conn: &DbConnection,
+    lobby_state: LobbyState,
+    alias: &str,
+) -> Result<CreateEmbed, CommandError> {
+    let embed_title = format!("{} (Lobby)", alias);
+    let players_nations = db_conn.players_with_nations_for_game_alias(&alias)?;
+    let registered_player_count = players_nations.len() as i32;
+
+    let mut player_names = String::new();
+    let mut nation_names = String::new();
+
+    for (player, nation_id) in players_nations {
+        let &(nation_name, era) = Nations::get_nation_desc(nation_id);
+        player_names.push_str(&format!("{}\n", player.discord_user_id.get()?));
+        nation_names.push_str(&format!("{} {}\n", era, nation_name));
+    }
+    for _ in 0..(lobby_state.player_count - registered_player_count) {
+        player_names.push_str(&".\n");
+        nation_names.push_str(&"OPEN\n");
+    }
+    let e = CreateEmbed::default()
+        .title(embed_title)
+        .field( |f| f
+            .name("Nation")
+            .value(nation_names)
+        )
+        .field ( |f| f
+            .name("Player")
+            .value(player_names)
+        );
+    Ok(e)
+}
+
+fn started_details(
+    db_conn: &DbConnection,
+    started_state: StartedState,
+    alias: &str,
+) -> Result<CreateEmbed, CommandError> {
+    let ref server_address = started_state.address;
+    let mut game_data = get_game_data(&server_address)?;
+    game_data.nations.sort_unstable_by(|a, b| a.name.cmp(&b.name));
+
+    let mut nation_names = String::new();
+    let mut player_names = String::new();
+    let mut submitted_status = String::new();
+
+    let id_player_nations = db_conn.players_with_nations_for_game_alias(&alias)?;
+
+    for nation in &game_data.nations {
+        debug!("Creating format for nation {} {}", nation.era, nation.name);
+        nation_names.push_str(&format!("{} {}\n", nation.era, nation.name));
+
+        let nation_string = if let NationStatus::Human = nation.status {
+            if let Some(&(ref player, _)) = id_player_nations.iter().find(
+                |&&(_, nation_id)| nation_id == nation.id
+                ) {
+                    format!("**{}**", player.discord_user_id.get()?)    
+                } else {
+                    nation.status.show().to_string()
+                }
+
+        } else {
+            nation.status.show().to_string()
+        };
+
+        player_names.push_str(&format!("{}\n", nation_string));
+        
+        if let NationStatus::Human = nation.status {
+            submitted_status.push_str(&format!("{}\n", nation.submitted.show()));
+        } else {
+            submitted_status.push_str(&".\n");
+        }
+    }
+    info!("Server details string created, now sending.");
+    let total_mins_remaining = game_data.turn_timer / (1000*60);
+    let hours_remaining = total_mins_remaining/60;
+    let mins_remaining = total_mins_remaining - hours_remaining*60;
+
+    let embed_title = format!("{}: turn {}, {}h {}m remaining",
+                game_data.game_name,
+                game_data.turn,
+                hours_remaining,
+                mins_remaining);
+
+    info!("replying with embed_title {:?}\n nations {:?}\n players {:?}\n, submission {:?}",
+    embed_title, nation_names, player_names, submitted_status);
+
+    let e = CreateEmbed::default()
+        .title(embed_title)
+        .field( |f| f
+            .name("Nation")
+            .value(nation_names)
+        )
+        .field ( |f| f
+            .name("Player")
+            .value(player_names)
+        )
+        .field ( |f| f
+            .name("Submitted")
+            .value(submitted_status)
+        );
+    Ok(e)
 }
---
+++ src/commands/servers/turn_check.rs
@@ -0,0 +1,68 @@
+use typemap::ShareMap;
+
+use db::{DbConnection, DbConnectionKey};
+use model::{GameServer, GameServerState};
+use model::enums::{NationStatus, SubmissionStatus};
+use std::{thread, time};
+use serenity::prelude::Mutex;
+use std::error::Error;
+use ::server;
+
+pub fn check_for_new_turns_every_1_min(mutex: &Mutex<ShareMap>) {
+    loop {
+        thread::sleep(time::Duration::from_secs(60));
+        info!("checking for new turns!");
+        message_players_if_new_turn(&mutex).unwrap_or_else(|e| {
+            error!("Checking for new turns failed with: {}", e);
+        });
+    }
+}
+
+fn message_players_if_new_turn(mutex: &Mutex<ShareMap>) -> Result<(), Box<Error>> {
+    let data = mutex.lock();
+    let db_conn = data.get::<DbConnectionKey>().ok_or("no db connection")?;
+    // TODO: transactions
+    let servers = db_conn.retrieve_all_servers()?;
+    for server in servers {
+        let server_name = server.alias.clone();
+        if let Err(err) = check_server_for_new_turn(server, &db_conn) {
+            println!("error checking {} for turn: {:?}", server_name, err);
+        };
+    }
+    Ok(())
+}
+
+fn check_server_for_new_turn(server: GameServer, db_conn: &DbConnection) -> Result<(), Box<Error>> {
+    if let GameServerState::StartedState(started_state) = server.state {
+        info!("checking {} for new turn", server.alias);
+        let game_data = server::get_game_data(&started_state.address)?;
+        let new_turn = db_conn.update_game_with_possibly_new_turn(
+            &server.alias,
+            game_data.turn
+        )?; 
+
+        if new_turn {
+            info!("new turn in game {}", server.alias);
+            for (player, nation_id) in db_conn.players_with_nations_for_game_alias(&server.alias)? {
+                if player.turn_notifications {
+                    // TODO: quadratic is bad. At least sort it..
+                    if let Some(nation) = game_data.nations.iter().find(|&nation| nation.id == nation_id) {
+                        if nation.status == NationStatus::Human && nation.submitted == SubmissionStatus::NotSubmitted {
+                            use model::enums::Nations;
+                            let &(name, era) = Nations::get_nation_desc(nation_id);
+                            let text = format!("your nation {} {} has a new turn ({}) in {}",
+                                era,
+                                name,
+                                game_data.turn,
+                                server.alias);
+                            info!("Sending DM: {}", text);
+                            let private_channel = player.discord_user_id.create_dm_channel()?;
+                            private_channel.say(&text)?;
+                    }
+                    }
+                }
+            }
+        }
+    };
+    Ok(())
+}
