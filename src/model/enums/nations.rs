5e0cd7141d6e80eef34013b123f501c7b6ea47bf
David McGillicuddy <contact@djmcgill.co.uk>
2018-02-28 15:52:01 +0000

    Add new nations

--- src/model/enums/nations.rs
+++ src/model/enums/nations.rs
@@ -76,7 +76,7 @@ lazy_static ! { // TODO: replace with PHF crate?
         m.insert(67u32, ("Nazca", Era::Middle));
         m.insert(68u32, ("Xibalba", Era::Middle));
         m.insert(69u32, ("Phlegra", Era::Middle)); // nice
-        m.insert(69u32, ("Phaeacia", Era::Middle));
+        m.insert(70u32, ("Phaeacia", Era::Middle));
         m.insert(73u32, ("Atlantis", Era::Middle));
         m.insert(74u32, ("R'lyeh", Era::Middle));
         m.insert(75u32, ("Pelagia", Era::Middle));
