e1782aef481b295e4f058c95ceda26eccef26977
David McGillicuddy <contact@djmcgill.co.uk>
2024-01-19 12:18:02 +0000

    unstart now removes cache entry too

--- src/commands/servers/unstart.rs
+++ src/commands/servers/unstart.rs
@@ -1,20 +1,32 @@
+use anyhow::anyhow;
 use serenity::{
     framework::standard::{Args, CommandError},
     prelude::Context,
 };
+use std::sync::Arc;
 
 use crate::commands::servers::CommandResponse;
 use crate::{
     commands::servers::alias_from_arg_or_channel_name, db::*, model::game_server::GameServerState,
+    DetailsCacheHandle, DetailsCacheKey,
 };
 use serenity::model::id::{ChannelId, UserId};
 
-fn unstart_helper(db_conn: DbConnection, alias: &str) -> Result<(), CommandError> {
+async fn unstart_helper(
+    db_conn: DbConnection,
+    handle: DetailsCacheHandle,
+    alias: &str,
+) -> Result<(), CommandError> {
     let server = db_conn.game_for_alias(alias)?;
 
     match server.state {
         GameServerState::StartedState(_, _) => {
             db_conn.remove_started_state(alias)?;
+            let mut guard = handle.0.write().await;
+            let write_handle = guard.get_mut::<DetailsCacheKey>().ok_or_else(|| {
+                anyhow!("Cache somehow not initialised, this should never happen!!")
+            })?;
+            let _ = write_handle.remove(alias);
         }
         GameServerState::Lobby(_) => {
             return Err(CommandError::from("cannot use this command on a lobby"))
@@ -29,6 +41,8 @@ pub async fn unstart(
     _user_id: UserId,
     mut args: Args,
 ) -> Result<CommandResponse, CommandError> {
+    let data_handle = DetailsCacheHandle(Arc::clone(&context.data));
+
     let db_conn = {
         let data = context.data.read().await;
         data.get::<DbConnectionKey>()
@@ -41,7 +55,7 @@ pub async fn unstart(
             "Too many arguments. TIP: spaces in arguments need to be quoted \"like this\"",
         ));
     }
-    unstart_helper(db_conn, &alias)?;
+    unstart_helper(db_conn, data_handle, &alias).await?;
     Ok(CommandResponse::Reply(format!(
         "Successfully turned '{}' back into a lobby",
         alias
