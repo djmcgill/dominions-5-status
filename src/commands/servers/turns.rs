9370c5fa2ba0bd74abbeb6f81bb8cf2b266d68cb
David McGillicuddy <contact@djmcgill.co.uk>
2024-02-20 00:08:55 +0000

    fix turns for too many games

--- src/commands/servers/turns.rs
+++ src/commands/servers/turns.rs
@@ -26,11 +26,12 @@ async fn turns_helper(
     user_id: UserId,
     db_conn: DbConnection,
     read_handle: DetailsCacheHandle,
-) -> Result<String, CommandError> {
+) -> Result<Vec<String>, CommandError> {
     debug!("Starting !turns");
+    let mut lines = vec![];
     let servers_and_nations_for_player = db_conn.servers_for_player(user_id)?;
 
-    let mut text = "Your turns:\n".to_string();
+    lines.push("Your turns:".to_string());
     let db_conn = &db_conn;
     for (server, _) in servers_and_nations_for_player {
         if let GameServerState::StartedState(started_state, option_lobby_state) = server.state {
@@ -48,8 +49,7 @@ async fn turns_helper(
                     match details.nations {
                         NationDetails::Started(started_state) => match started_state.state {
                             StartedStateDetails::Uploading(uploading_state) => {
-                                turns_for_uploading_state(
-                                    &mut text,
+                                let mut texts = turns_for_uploading_state(
                                     &uploading_state,
                                     user_id,
                                     &server.alias,
@@ -58,10 +58,10 @@ async fn turns_helper(
                                         .and_then(|cache_entry| cache_entry.option_snek_state)
                                         .as_ref(),
                                 );
+                                lines.append(&mut texts);
                             }
                             StartedStateDetails::Playing(playing_state) => {
-                                turns_for_playing_state(
-                                    &mut text,
+                                let mut texts = turns_for_playing_state(
                                     &playing_state,
                                     user_id,
                                     &server.alias,
@@ -70,27 +70,28 @@ async fn turns_helper(
                                         .and_then(|cache_entry| cache_entry.option_snek_state)
                                         .as_ref(),
                                 );
+                                lines.append(&mut texts);
                             }
                         },
                         NationDetails::Lobby(_) => continue,
                     }
                 }
                 Err(err) => {
-                    text.push_str(&format!("{}: ERROR {}\n", server.alias, err));
+                    lines.push(format!("{}: ERROR {}", server.alias, err));
                 }
             }
         }
     }
-    Ok(text)
+    Ok(lines)
 }
 
 fn turns_for_uploading_state(
-    text: &mut String,
     uploading_state: &UploadingState,
     user_id: UserId,
     alias: &str,
     option_snek_state: Option<&SnekGameStatus>,
-) {
+) -> Vec<String> {
+    let mut texts = vec![];
     let player_count = uploading_state.uploading_players.len();
     let uploaded_player_count = uploading_state
         .uploading_players
@@ -113,14 +114,14 @@ fn turns_for_uploading_state(
                 //        does it fail only when people are registered multiple times?
                 if registered_player.discord_user_id == user_id {
                     let turn_str = format!(
-                        "{} uploading: {} (uploaded: {}, {}/{})\n",
+                        "{} uploading: {} (uploaded: {}, {}/{})",
                         alias,
                         player_details.nation_identifier.name(option_snek_state),
                         SubmissionStatus::Submitted.show(),
                         uploaded_player_count,
                         player_count,
                     );
-                    text.push_str(&turn_str);
+                    texts.push(turn_str);
                 }
             }
             // If this is them, they haven't uploaded
@@ -129,27 +130,28 @@ fn turns_for_uploading_state(
                 //        does it fail only when people are registered multiple times?
                 if registered_player.discord_user_id == user_id {
                     let turn_str = format!(
-                        "{} uploading: {} (uploaded: {}, {}/{})\n",
+                        "{} uploading: {} (uploaded: {}, {}/{})",
                         alias,
                         registered_nation_identifier.name(option_snek_state),
                         SubmissionStatus::NotSubmitted.show(),
                         uploaded_player_count,
                         player_count,
                     );
-                    text.push_str(&turn_str);
+                    texts.push(turn_str);
                 }
             }
         }
     }
+    texts
 }
 
 fn turns_for_playing_state(
-    text: &mut String,
     playing_state: &PlayingState,
     user_id: UserId,
     alias: &str,
     option_snek_state: Option<&SnekGameStatus>,
-) {
+) -> Vec<String> {
+    let mut texts = vec![];
     let (playing_players, submitted_players) =
         count_playing_and_submitted_players(&playing_state.players[..]);
 
@@ -166,7 +168,7 @@ fn turns_for_playing_state(
                     let deadline = discord_date_format(playing_state.turn_deadline);
 
                     let turn_str = format!(
-                        "{} turn {} ({}): {} (submitted: {}, {}/{})\n",
+                        "{} turn {} ({}): {} (submitted: {}, {}/{})",
                         alias,
                         playing_state.turn,
                         deadline,
@@ -177,11 +179,12 @@ fn turns_for_playing_state(
                         submitted_players,
                         playing_players,
                     );
-                    text.push_str(&turn_str);
+                    texts.push(turn_str);
                 }
             }
         }
     }
+    texts
 }
 
 fn count_playing_and_submitted_players(players: &[PotentialPlayer]) -> (u32, u32) {
@@ -224,9 +227,14 @@ pub async fn turns(
             .ok_or_else(|| CommandError::from("No db connection"))?
             .clone()
     };
-    let text = turns_helper(user_id, db_conn, read_handle).await?;
-    info!("turns: replying with: {}", text);
+    let lines = turns_helper(user_id, db_conn, read_handle).await?;
+    let messages = lines.chunks(20);
     let private_channel = user_id.create_dm_channel(&context.http).await?;
-    private_channel.say(&context.http, &text).await?;
+
+    for message_lines in messages {
+        let text = message_lines.join("\n");
+        info!("turns: replying with: {}", text);
+        private_channel.say(&context.http, &text).await?;
+    }
     Ok(CommandResponse::Reply("DM sent".to_owned()))
 }
